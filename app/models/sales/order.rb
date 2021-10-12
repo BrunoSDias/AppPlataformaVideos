7bdeb752c2c6f8f3e52c3edbf7c469d98e843048
bruno <brunosdias@outlook.com>
2021-10-11 22:01:14 -0300

    After Commit Create UsersCourse

--- app/models/sales/order.rb
+++ app/models/sales/order.rb
@@ -3,4 +3,18 @@ class Sales::Order < ApplicationRecord
   belongs_to :sales_course, class_name: "Sales::Course", foreign_key: :sales_course_id
 
   enum status: { aguardando: 1, pago: 2 }
+
+  after_commit :create_users_course
+
+  def create_users_course
+    return if Client::UsersCourse.find_by(client_user_id: client_user_id, sales_course_id: sales_course_id)
+    return unless self.status == "pago"
+    
+    course = self.sales_course
+    if Client::UsersCourse.create(client_user_id: client_user_id, sales_course_id: course.id)
+      course.sales_videos.each do |video|
+        Client::UserVideo.create(client_user_id: client_user_id, sales_video_id: video.id)
+      end
+    end
+  end
 end
