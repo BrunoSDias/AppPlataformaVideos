8e9c08672603c9cf42370a56334afc119a1e9e86
bruno <brunosdias@outlook.com>
2021-10-11 21:34:00 -0300

    added host by environment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authorize
+  before_action :set_host
 
   def authorize
     if cookies[:user].present?
@@ -10,5 +11,13 @@ class ApplicationController < ActionController::Base
       end
     end
     redirect_to root_path
+  end
+
+  def set_host
+    if Rails.env.production
+      @host ||= "https://app-courses-platform.herokuapp.com/"
+    else
+      @host ||= "http://localhost:3000"
+    end
   end
 end
